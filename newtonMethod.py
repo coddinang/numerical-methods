1c03d1781f7ab50d766d99d98462ab398f2559e5
addleonel <cambroni3633@gmail.com>
2021-09-20 21:52:05 -0500

    __main__ section defined

--- newtonMethod.py
+++ newtonMethod.py
@@ -1,26 +1,33 @@
 
 import math
 
-def functionOriginal(x):
-    f = math.cos(x) -math.pow(x, 3)
-    return f
-
-def functionDerived(x):
-    f_d = -math.sin(x) -3*x**2
-    return f_d
-
-def runMethod(x_0):
+def newton_method(x_0, iter_to, func_original, func_derived):
     iterator = 1
     while True:
-        f = functionOriginal(x_0)
-        f_d = functionDerived(x_0)
-        x_next = x_0 -(f/f_d)
+        f = func_original(x_0)
+        f_d = func_derived(x_0)
+        x_next = x_0 - (f/f_d)
 
         print("x_{} = {}".format(iterator, x_next))
-        if iterator == 6:
+        if iterator == iter_to:
             break
         x_0 = x_next
-        iterator = iterator + 1
+        iterator += 1
 
-# run
-runMethod(0.5)
+
+if __name__ == '__main__':
+
+    def function_original(x):
+        """
+        This is the original function: f(x) = cos(x) - x^3
+        """
+        return math.cos(x) - math.pow(x, 3)
+
+
+    def function_derived(x):
+        """
+        This is the derived function: f(x)' = -sin(x) - 3*x^2
+        """
+        return -math.sin(x) - 3 * x ** 2
+
+    newton_method(0.5, 6, function_original, function_derived)
